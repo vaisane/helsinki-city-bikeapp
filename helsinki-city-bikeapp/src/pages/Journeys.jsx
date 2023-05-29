2ff23fbea63a6319e25ee77641664e3c7cf52674
vaisane <vaisane.jani@gmail.com>
2023-05-30 01:49:03 +0300

    Journey list view

--- helsinki-city-bikeapp/src/pages/Journeys.jsx
+++ helsinki-city-bikeapp/src/pages/Journeys.jsx
@@ -1,7 +1,58 @@
 import React from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const columns = [
+  {
+    field: "Departure station name",
+    headerName: "Departure station",
+    flex: 2,
+  },
+  { field: "Return station name", headerName: "Return station", flex: 2 },
+  { field: "Covered distance (m)", headerName: "Distance (km)", flex: 1 },
+  { field: "Duration (sec)", headerName: "Duration (min)", flex: 1 },
+];
 
 const JourneyView = () => {
-  return <div>journeys</div>;
+  const [journeys, setJourneys] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [totalPages, setTotalPages] = useState(0);
+  const [paginationModel, setPaginationModel] = useState({
+    pageSize: 25,
+    page: 1,
+  });
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(
+        `http://127.0.0.1:4000/journeys/?page=${paginationModel.page}&limit=${paginationModel.pageSize}`
+      )
+      .then((response) => {
+        setJourneys(response.data.result);
+        setTotalPages(response.data.totalPages);
+        setLoading(false);
+      });
+  }, [paginationModel]);
+
+  return (
+    <>
+      <h2>Journey list</h2>
+      <DataGrid
+        rows={journeys}
+        getRowId={(row) => row["_id"]}
+        columns={columns}
+        paginationModel={paginationModel}
+        onPaginationModelChange={setPaginationModel}
+        paginationMode="server"
+        rowCount={totalPages}
+        rowsPerPageOptions={[25, 50, 100]}
+        autoHeight={true}
+        loading={loading}
+      />
+    </>
+  );
 };
 
 export default JourneyView;
